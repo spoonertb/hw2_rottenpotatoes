b5cb099ad0927b364f5f322bede75b09454f1343
Tom Spooner <thomas.spooner@colorado.edu>
2013-10-15 20:41:37 -0700

    Added some comments

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,22 +11,26 @@ class MoviesController < ApplicationController
     @sort = params[:sort] || session[:sort]
     @ratings = params[:ratings] || session[:ratings]
 
-
+    #Provides a way to redirect the page to that of the saved session when the 
+    #params hash and ratings hash are nil
     if params[:sort].nil? && params[:ratings].nil? &&
       (!session[:sort].nil? || !session[:ratings].nil?)
       redirect_to :sort => @sort, :ratings => @ratings and return
     end
-    
+
+    # Provides a default selection when the page is loaded/nil
     if @ratings.nil?
       @ratings = {"G"=>1,"PG"=>1,"PG-13"=>1,"R"=>1}
     end
 
+    # If a sort was selected by the user, sort the selected movies on it.
     if !@sort.nil?
       @movies = Movie.order("#{@sort} ASC").find_all_by_rating(@ratings.keys)
     else
       @movies = Movie.find_all_by_rating(@ratings.keys)
     end
 
+    # Saves the current rating/sort config to the session after everything else
     session[:ratings] = @ratings
     session[:sort] = @sort
 
